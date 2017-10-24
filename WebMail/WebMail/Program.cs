edc7c5a7ab2bea41507c61554b9e8192c52a83e6
TomaszC <tomcwiko@student.pg.gda.pl>
2017-10-24 23:51:49 +0200

    Add DownloadMails() first prototype

--- WebMail/WebMail/Program.cs
+++ WebMail/WebMail/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using WebMail.Models;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Security;
+using MailKit.Search;
 
 namespace WebMail
 {
@@ -33,6 +38,8 @@ namespace WebMail
                 }
             }
 
+            DownloadMails();
+
             host.Run();
         }
 
@@ -40,5 +47,35 @@ namespace WebMail
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+        public static void DownloadMails()
+        {
+            Console.WriteLine("\n========== DownloadMails() BEGIN ==========\n");
+            try
+            {
+                ImapClient imapClient = new ImapClient();
+                imapClient.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+                imapClient.Authenticate("webmail2017.dev", "12341234xx");
+
+                imapClient.Inbox.Open(FolderAccess.ReadOnly);
+                var uids = imapClient.Inbox.Search(SearchQuery.All);
+
+                foreach (var uid in uids)
+                {
+                    var message = imapClient.Inbox.GetMessage(uid);
+                    Console.WriteLine("\n{0} -------------------------", uid);
+                    Console.WriteLine("From: {0}", message.From);
+                    Console.WriteLine("Subject: {0}", message.Subject);
+                    //Console.WriteLine("TextBody: {0}\n", message.TextBody); // uncomment to print whole message content
+                }
+
+                imapClient.Disconnect(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while trying to download mails: {0}", ex.Message);
+            }
+            Console.WriteLine("\n=========== DownloadMails() END ===========\n");
+        }
     }
 }
