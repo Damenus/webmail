2ad641f7c21fd734c6294e227e8828f6de32b610
TomaszC <tomcwiko@student.pg.gda.pl>
2017-11-04 12:25:08 +0100

    Modified Mails entity, add UpdateInboxMessages() function

--- Server/Controllers/api/MailController.cs
+++ Server/Controllers/api/MailController.cs
@@ -10,6 +10,13 @@ using Microsoft.Extensions.Logging;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit.Security;
+using MimeKit;
 
 namespace WebMail.Server.Controllers.api
 {
@@ -54,6 +61,44 @@ namespace WebMail.Server.Controllers.api
         public IEnumerable<Mail> GetMails()
         {
             return _dbContext.Mails;
+        }
+
+        public bool UpdateInboxMessages()
+        {
+            try
+            {
+                ImapClient imapClient = new ImapClient();
+                imapClient.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+                imapClient.Authenticate("webmail2017.dev", "12341234xx");
+                imapClient.Inbox.Open(FolderAccess.ReadOnly);
+                var uidsFromServer = imapClient.Inbox.Search(SearchQuery.All);
+                var uidsFromDB = GetMails().Select(m => m.UniqueID);
+
+                foreach (UniqueId uid in uidsFromServer)
+                {
+                    if (!uidsFromDB.Contains(uid.Id))
+                    {
+                        MimeMessage message = imapClient.Inbox.GetMessage(uid);
+                        _dbContext.Mails.Add(new Mail
+                        {
+                            UniqueID = uid.Id,
+                            Sender = message.From.ToString(),
+                            Title = message.Subject,
+                            Body = message.TextBody,
+                            Date = message.Date
+                        });
+                        _dbContext.SaveChanges();
+                    }
+                }
+
+                imapClient.Disconnect(true);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while trying to update inbox mails: {0}", ex.Message);
+                return false;
+            }
         }
     }
 }
