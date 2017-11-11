a88b0c439a011de6ad5ef64dcb06a502eb232647
teosl <teosle@gmail.com>
2017-11-11 23:12:02 +0100

    add disable smtp and imap input when find in our list

--- ClientApp/app/modal/addImap.component.ts
+++ ClientApp/app/modal/addImap.component.ts
@@ -19,25 +19,37 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class addImap {
 
     constructor(public activeModal: NgbActiveModal, public dataService: DataService) { }
+
+    public servers: any;
+
+    public ngOnInit() {
+        this.servers = require('./servers.json');
+    }
     onSubmit(form: any) {
-        console.log("adding form values ");
-        console.log(form.value);
         console.log(this.dataService.post('api/bla', form.value));
     }
     onEmailChange() {
         var email: string;
         var domain: string;
-        var servers = require('./servers.json');
+        var founded: boolean;
 
         email = (document.getElementById("MailAdress") as HTMLInputElement).value;
 
         if (email.indexOf("@") !== -1) {
+            founded = false;
             domain = email.substring(email.lastIndexOf("@") + 1);
-            for (var server in servers) {
+            for (var server in this.servers) {
                 if (server == domain) {
-                    (document.getElementById("SmtpServerAdress") as HTMLInputElement).value = servers[server].smtp;
-                    (document.getElementById("ImapServerAdress") as HTMLInputElement).value = servers[server].imap;
+                    (document.getElementById("SmtpServerAdress") as HTMLInputElement).value = this.servers[server].smtp;
+                    (document.getElementById("SmtpServerAdress") as HTMLInputElement).disabled = true;
+                    (document.getElementById("ImapServerAdress") as HTMLInputElement).value = this.servers[server].imap;
+                    (document.getElementById("ImapServerAdress") as HTMLInputElement).disabled = true;
+                    founded = true;
                 }
+            }
+            if (!founded) {
+                (document.getElementById("SmtpServerAdress") as HTMLInputElement).disabled = false;
+                (document.getElementById("ImapServerAdress") as HTMLInputElement).disabled = false;
             }
         }
         else {
