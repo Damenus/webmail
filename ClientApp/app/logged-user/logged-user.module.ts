f813a8ad3f0a6b09fb7dbe303e1526893e01e3fd
Franciszek Zygmunt <franciszek.zygmunt@gmail.com>
2017-11-12 00:22:52 +0100

    sidebar works

--- ClientApp/app/logged-user/logged-user.module.ts
+++ ClientApp/app/logged-user/logged-user.module.ts
@@ -3,9 +3,9 @@ import { LoggedUserComponent } from './logged-user.component';
 import { routing } from './logged-user.routes';
 import { CommonModule } from "@angular/common";
 import { SharedModule } from '../shared/shared.module';
-
+import { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
 @NgModule({
     imports: [routing, CommonModule, SharedModule],
-    declarations: [LoggedUserComponent]
+    declarations: [LoggedUserComponent, SidebarLeftComponent]
 })
 export class LoggedUserModule { }
---
+++ ClientApp/app/logged-user/sidebar-left/sidebar-left.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'appc-sidebar-left',
+  templateUrl: './sidebar-left.component.html',
+  styleUrls: ['./sidebar-left.component.scss']
+})
+export class SidebarLeftComponent {
+
+  constructor() { }
+
+}
