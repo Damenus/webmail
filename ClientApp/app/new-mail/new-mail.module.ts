27c1d7e83519d59359c5c9a62b63ee4c5e1320db
Franciszek Zygmunt <franciszek.zygmunt@gmail.com>
2017-11-13 20:16:03 +0100

    added translations for new mail view

--- ClientApp/app/new-mail/new-mail.module.ts
+++ ClientApp/app/new-mail/new-mail.module.ts
@@ -6,13 +6,15 @@ import { NewMailRoutingModule } from './new-mail-routing.module';
 import { NewMailComponent } from './new-mail.component';
 import { NewMailService } from './new-mail.service'
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     NgbModule,
     NewMailRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   declarations: [
     NewMailComponent
