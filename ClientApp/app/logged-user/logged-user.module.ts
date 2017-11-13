55dbbfb26e6f48776203245157f0f4e6b72e186f
Franciszek Zygmunt <franciszek.zygmunt@gmail.com>
2017-11-13 17:53:47 +0100

    added right sidebar

--- ClientApp/app/logged-user/logged-user.module.ts
+++ ClientApp/app/logged-user/logged-user.module.ts
@@ -4,8 +4,9 @@ import { routing } from './logged-user.routes';
 import { CommonModule } from "@angular/common";
 import { SharedModule } from '../shared/shared.module';
 import { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
+import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
 @NgModule({
     imports: [routing, CommonModule, SharedModule],
-    declarations: [LoggedUserComponent, SidebarLeftComponent]
+    declarations: [LoggedUserComponent, SidebarLeftComponent, SidebarRightComponent]
 })
 export class LoggedUserModule { }
---
+++ ClientApp/app/logged-user/sidebar-right/sidebar-right.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'appc-sidebar-right',
+  templateUrl: './sidebar-right.component.html',
+  styleUrls: ['./sidebar-right.component.scss']
+})
+export class SidebarRightComponent {
+
+    constructor() { }
+
+}
