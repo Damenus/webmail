526d4f85c7e7336d6d39d28115d1645916e651c8
Damenus <damian.darczuk@gmail.com>
2017-12-03 21:07:08 +0100

    Add simple tests

---
+++ tests/UnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+using WebMail.Server.Controllers.api;
+
+namespace WebMailTests
+{
+    public class UnitTests
+    {
+        public MailController mailController;
+
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(1 == 1);
+            
+        }
+
+        [Fact]
+        public void TestMethod()
+        {
+           Assert.False(1 == 2);
+        }
+    }
+}
