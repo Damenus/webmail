b05ae34269cfb164fb4e1ac23cc9329ab6ed5fd8
Jakub Jarząbek <jarzabek.kuba@gmail.com>
2017-12-03 21:11:23 +0100

    Fixed migration

--- Migrations/20171203200656_DraftsMigration2.Designer.cs
+++ Migrations/20171203200656_DraftsMigration2.Designer.cs
@@ -11,9 +11,10 @@ using WebMail.Server;
 namespace WebMail.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171203200656_DraftsMigration2")]
+    partial class DraftsMigration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20171203200656_DraftsMigration2.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebMail.Migrations
+{
+    public partial class DraftsMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Attachment",
+                table: "Mails",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AttachmentName",
+                table: "Mails",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Receiver",
+                table: "Mails",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Drafts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Attachment = table.Column<string>(type: "TEXT", nullable: true),
+                    AttachmentName = table.Column<string>(type: "TEXT", nullable: true),
+                    Body = table.Column<string>(type: "TEXT", nullable: true),
+                    Date = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    Receiver = table.Column<string>(type: "TEXT", nullable: true),
+                    Sender = table.Column<string>(type: "TEXT", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    UniqueID = table.Column<uint>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drafts", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Drafts");
+
+            migrationBuilder.DropColumn(
+                name: "Attachment",
+                table: "Mails");
+
+            migrationBuilder.DropColumn(
+                name: "AttachmentName",
+                table: "Mails");
+
+            migrationBuilder.DropColumn(
+                name: "Receiver",
+                table: "Mails");
+        }
+    }
+}
